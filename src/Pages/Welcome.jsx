bc6c4e61791d2c90ab5b69d3a018a78bdad3e2e2
Leonardo <leonardocastillo1410@gmail.com>
2023-08-08 21:11:46 -0400

    Revert "ticket 7 formulario login"
    This reverts commit 5972a9eea03f93eece55aa30d1510fe8e4b48795.

--- src/Pages/Welcome.jsx
+++ src/Pages/Welcome.jsx
@@ -1,23 +1,5 @@
-
-
-export const Welcome = () => {
-  return (
-    
-     <>
-    <div className="header">
-    <h1>Bienvenido a Denticare</h1>
-    <p>Your Healthy Smile is Our Priority</p>
-  </div>
-  
-  <div class="cover-image">
-    <div class="cover-content">
-      <h2>Your Trusted Dental Care Partner</h2>
-      <p>We are committed to providing high-quality dental services to our patients.</p>
-      <a href="#" class="cta-button">Book an Appointment</a>
-     
-    </div>
-  </div>
-  </>
-  );
-};
-  
+export const Welcome = ()=>{
+    return(
+        <h1>WELCOME</h1>
+    )
+}
