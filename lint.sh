a2926e5a7ff8860598bf33087cb05288040274e8
Rafael <luxrafacm@gmail.com>
2024-05-24 20:19:55 -0400

    added linter

---
+++ lint.sh
@@ -0,0 +1,11 @@
+ if [ -n "$(git diff --name-only --cached --diff-filter=d -- '*.tsx' '*.ts' '*.jsx' '*.js')" ]; then
+     has_errors=false
+     git diff --name-only --cached --diff-filter=d -- '*.tsx' '*.ts' '*.jsx'  | while read -r file; do
+         prettier --write "$file" || { echo "Prettier encountered an error while formatting $file"; has_errors=true; }
+         git add .
+     done
+     if [ "$has_errors" = true ]; then
+         echo "Prettier encountered errors. Aborting commit."
+         exit 1
+     fi
+ fi
