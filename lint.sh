15afe3d571df28425c7e421083208dddc9709c2e
Rafael <luxrafacm@gmail.com>
2024-05-24 20:24:09 -0400

    11

--- lint.sh
+++ lint.sh
@@ -1,9 +1,9 @@
  if [ -n "$(git diff --name-only --cached --diff-filter=d -- '*.tsx' '*.ts' '*.jsx' '*.js')" ]; then
      has_errors=false
      git diff --name-only --cached --diff-filter=d -- '*.tsx' '*.js' '*.jsx'  | while read -r file; do
-         prettier --write "$file" || { echo "Prettier encountered an error while formatting $file"; has_errors=true; }
+         yarn prettier --write "$file" || { echo "Prettier encountered an error while formatting $file"; has_errors=true; }
          git add .
-         exit 1
+         exit 0
      done
      if [ "$has_errors" = true ]; then
          echo "Prettier encountered errors. Aborting commit."
--- src/Pages/Patients.jsx
+++ src/Pages/Patients.jsx
@@ -1,40 +1,37 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Button, ButtonGroup, Card, Table } from "react-bootstrap";
 import { PatientForm } from "../Forms/Patient";
 import { ApiClient } from "../api/ApiClient";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export const Patients = () => {
-  const [patients, setPatients] = useState([])
+  const [patients, setPatients] = useState([]);
   const refresh = useCallback(() => {
-    ApiClient.patients.all().then(res => {
-      setPatients(res)
-    })
-  }, [])
+    ApiClient.patients.all().then((res) => {
+      setPatients(res);
+    });
+  }, []);
 
   const deletePatient = (id) => {
-    if (window.confirm('¿Estas seguro de eliminar este paciente?')) {
-      ApiClient.patients.delete(id).then(res => {
-        refresh()
-      })
+    if (window.confirm("¿Estas seguro de eliminar este paciente?")) {
+      ApiClient.patients.delete(id).then((res) => {
+        refresh();
+      });
     }
-  }
+  };
   useEffect(() => {
-    refresh()
-  }, [])
+    refresh();
+  }, []);
   return (
     <div className="patients">
-
       <div className="d-flex justify-content-between align-items-center">
         <h2>Pacientes </h2>
-
-
 
         <PatientForm onCreate={refresh} />
       </div>
 
-      <Table size='sm' striped>
+      <Table size="sm" striped>
         <thead>
           <tr>
             <th>CI</th>
@@ -42,37 +39,45 @@ export const Patients = () => {
 
             <th>Edad</th>
             <th>Telefono</th>
-            <th style={{ width: '20%' }}>Acciones</th>
+            <th style={{ width: "20%" }}>Acciones</th>
           </tr>
         </thead>
         <tbody>
-          {
-            patients.map((patient, index) => (
-              <tr>
-                <th scope="row">{patient.ci}</th>
-                <td>{patient.name}</td>
-                <td>{patient.age}</td>
-                <td>{patient.phone}</td>
-                <td>
-                  <ButtonGroup>
-                    <Button as={Link} to={`/patients/${patient.id}`} size='sm' variant="outline-success"><FontAwesomeIcon icon='fa fa-eye' className='me-2' />Ver</Button>
-                    <Button size='sm' variant="outline-primary"><FontAwesomeIcon icon='fa fa-pencil' className='me-2' />Editar</Button>
-                    <Button size='sm' variant="outline-danger" onClick={() => deletePatient(patient.id)}><FontAwesomeIcon icon='fa fa-eye' className='me-2' />Eliminar</Button>
-                  </ButtonGroup>
-                </td>
-              </tr>
-            ))
-          }
-
-
+          {patients.map((patient, index) => (
+            <tr>
+              <th scope="row">{patient.ci}</th>
+              <td>{patient.name}</td>
+              <td>{patient.age}</td>
+              <td>{patient.phone}</td>
+              <td>
+                <ButtonGroup>
+                  <Button
+                    as={Link}
+                    to={`/patients/${patient.id}`}
+                    size="sm"
+                    variant="outline-success"
+                  >
+                    <FontAwesomeIcon icon="fa fa-eye" className="me-2" />
+                    Ver
+                  </Button>
+                  <Button size="sm" variant="outline-primary">
+                    <FontAwesomeIcon icon="fa fa-pencil" className="me-2" />
+                    Editar
+                  </Button>
+                  <Button
+                    size="sm"
+                    variant="outline-danger"
+                    onClick={() => deletePatient(patient.id)}
+                  >
+                    <FontAwesomeIcon icon="fa fa-eye" className="me-2" />
+                    Eliminar
+                  </Button>
+                </ButtonGroup>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </Table>
     </div>
-
-
-
   );
 };
-
-
-
